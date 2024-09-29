0597266f4696b4f3aa8ce224634cf84da334f4a4
RonxldWilson <ronaldwilson00000@gmail.com>
2024-09-29 09:38:13 +0530

    Backend added for Todo along with env variable protection

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,69 @@
 const express = require("express");
+const {createTodo, updateTodo} = require('./types');
+const { todo } = require("./db");
+const cors = require("cors");
+
 const app = express();
 
 const port = 3000;
 
 app.use(express.json());
 
+app.use(cors);
+
 app.use((err,req,res,next)=>{
 	
 })
 
-app.post('/todo',(req,res)=>{
+app.post('/todo',async function (req,res){
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg: "wrong data inputs",
+        })
+        return;
+    }
+    await todo.create({
+         title: createPayload.title,
+         description: createPayload.description,
+         completed: createPayload.completed
+
+})
+    res.json({
+        msg: "Added Successfully"
+    })
 
 });
 
-app.get('/todos',(req,res)=>{
-	
+app.get('/todos',async function (req,res){
+	const todos = await todo.find();
+
+    res.json({
+        todos: []
+    })
 });
 
-app.put('/completed',(req,res)=>{
+app.put('/completed',async function(req,res){
+    const updatePayload = req.body;
+    const parsePayload = updateTodo.safeParse(updatePayload);
 
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg: "wrong data inputs",
+        })
+        return;
+    }
+
+    await todo.update({
+        _id: req.body.id
+    },{
+        completed:true
+    })
+    res.json({
+        msg: "Todo is done"
+    })
 });
 
 app.delete('/',(req,res)=>{
